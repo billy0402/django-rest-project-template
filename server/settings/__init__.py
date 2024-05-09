200477e38a8a2253087a2f0d863ace10c66f4552
Billy Huang <10646003@ntub.edu.tw>
2024-05-09 13:44:25 +0800

    feat(settings): setup whitenoise

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -162,6 +163,12 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = f"{URL_PREFIX}media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
