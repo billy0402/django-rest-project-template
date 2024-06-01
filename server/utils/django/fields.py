1ed2815783302b30eb84c872e0a12e0fca2c868b
Billy Huang <10646003@ntub.edu.tw>
2024-06-01 17:10:49 +0800

    feat(utils): TimestampModel use custom fields to make updated_at as same as created_at when object is created

--- server/utils/django/fields.py
+++ server/utils/django/fields.py
@@ -1,3 +1,4 @@
+import datetime
 import uuid
 from typing import Any
 
@@ -12,3 +13,27 @@ class UUIDAutoField(models.UUIDField, models.AutoField):
         kwargs.setdefault("default", uuid.uuid4)
         kwargs.setdefault("editable", False)
         super().__init__(*args, **kwargs)
+
+
+class CreatedAtField(models.DateTimeField):
+    def __init__(self, *args: list[Any], **kwargs: dict[str, Any]):
+        kwargs["auto_now_add"] = True
+        super().__init__(*args, **kwargs)
+
+
+class UpdatedAtField(models.DateTimeField):
+    def __init__(self, *args: list[Any], **kwargs: dict[str, Any]):
+        kwargs["auto_now"] = True
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance: models.Model, add: bool) -> datetime.datetime:
+        if add:
+            for field in model_instance._meta.get_fields():
+                if not isinstance(field, CreatedAtField):
+                    continue
+
+                value = getattr(model_instance, field.name)
+                setattr(model_instance, self.attname, value)
+                return value
+
+        return super().pre_save(model_instance, add)
--- server/utils/django/models.py
+++ server/utils/django/models.py
@@ -3,6 +3,8 @@ import uuid
 from django.conf import settings
 from django.db import models
 
+from server.utils.django import fields as util_fields
+
 
 class UUIDModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -12,8 +14,8 @@ class UUIDModel(models.Model):
 
 
 class TimestampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = util_fields.CreatedAtField()
+    updated_at = util_fields.UpdatedAtField()
 
     class Meta:
         abstract = True
