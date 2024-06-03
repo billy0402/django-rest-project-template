fd58a6cc50fae51fda8b05e6108f7070d4605eeb
Billy Huang <10646003@ntub.edu.tw>
2024-06-03 14:25:19 +0800

    fix(authentication): CustomUserAdmin should inheritance from UserAdmin

--- server/app/authentication/admin.py
+++ server/app/authentication/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 
 from server.app.authentication import models as auth_models
 
 
 @admin.register(auth_models.CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(auth_admin.UserAdmin):
     pass
