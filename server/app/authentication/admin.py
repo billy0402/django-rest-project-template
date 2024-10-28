0398e0da9bb3fd368e4714c1f656c0ee8e3ddd31
Billy Huang <10646003@ntub.edu.tw>
2024-10-28 15:18:14 +0800

    feat(authentication): admin user add user action log

--- server/app/authentication/admin.py
+++ server/app/authentication/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
+from django.utils.translation import gettext_lazy as _
 
 from server.app.authentication import models as auth_models
+from server.utils.django import admin as util_admin
 
 
 @admin.register(auth_models.User)
 class UserAdmin(auth_admin.UserAdmin):
-    pass
+    fieldsets = (
+        *auth_admin.UserAdmin.fieldsets,
+        (_("User action log"), {"fields": util_admin.BaseAdmin.readonly_fields}),  # pyright: ignore[reportAssignmentType]
+    )
+    readonly_fields = (
+        *util_admin.BaseAdmin.readonly_fields,
+        "last_login",
+        "date_joined",
+    )
