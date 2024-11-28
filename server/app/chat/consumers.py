aa5c1ac788083c137d72dce4554a4cb16896bb2a
Billy Huang <10646003@ntub.edu.tw>
2024-11-28 17:40:38 +0800

    feat(chat): implement a chat server

--- server/app/chat/consumers.py
+++ server/app/chat/consumers.py
@@ -1,17 +1,41 @@
 import json
+import typing as t
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self) -> None:
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code: str) -> None:
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
+    # Receive message from WebSocket
     def receive(self, text_data: str) -> None:
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event: dict[str, t.Any]) -> None:
+        message = event["message"]
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
